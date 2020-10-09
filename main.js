6377f0cf3973a8322f298c63bcaea312db16eb9e
Boris Postma <Boris@T-2000.local>
2020-10-09 14:25:00 +0200

    project start

---
+++ main.js
@@ -0,0 +1,14 @@
+const info = document.getElementById("contact-info");
+const contact = document.getElementById("contact");
+
+
+info.onclick = () => {
+    info.style.display = "none";  
+}
+
+contact.onclick = () => {
+    info.style.display = "block";
+}
+
+
+
